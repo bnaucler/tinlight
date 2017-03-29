5f4dbaa8b69e60ed92486365d9c994770e7efccb
bnaucler <mail@bnaucler.se>
2017-03-29 12:05:39 +0200

    Syntax and optimization

--- src/sketch.ino
+++ src/sketch.ino
@@ -7,35 +7,26 @@
 	http://etsy.com/shop/Tinkerlight
 	http://bnaucler.se
 
-	Absolutely no rights reserved.
-	Feel free to copy and modify the code to your liking.
-	I'm curious to see what you come up with!
-
-	TODO:
-	* More modes!
-		Disco?
-	* Enum for brightness levels?
-	* Improve flame mode by individual pixel movement?
-		Or merge with glow?
-	* Reduce code footprint for 'twinkle' and 'increase'
+	License: MIT
+	See README.md for more information
 
 */
 
 #include <Adafruit_NeoPixel.h>
 
 // Pin definitions
-const int		pixelPin = 7;
-const int		sw1Pin = 2;
-const int		sw2Pin = 8;
+const int		pixelPin = 2;
+const int		sw1Pin = 8;
+const int		sw2Pin = 11;
 
 // Global constants
 const int		modes = 4;					// Number of modes - static
 const int		numPixels = 13;				// Number of pixels - static
 const int		maxBrightness = 255;
 const int		minBrightness = 0;
-const int		minColorIntensity = 150;	// When randomly selecting color 
+const int		minColorIntensity = 150;	// When randomly selecting color
 
-const boolean	moreRandom = true;			// Set to true for more color change
+const bool		moreRandom = true;			// Set to true for more color change
 
 // 'Worm' mode constants
 const int		wormInterval = 300;			// Worm speed
@@ -60,7 +51,7 @@ const int		twinkleInterval = 10;		// Twinkle speed
 const int		twinklePauseInterval = 100;	// Time to pause at max and min
 
 // Switch states
-boolean			sw1Read, sw2Read;
+bool			sw1Read, sw2Read;
 
 // Variable declarations
 int				currentMode = 1, previousMode = 2;
@@ -73,15 +64,15 @@ int				twinkleCurrentPixel;
 int				flameInterval;
 int				glowDiv[numPixels];
 
-boolean			glowIncrease, glowDoChange;
-boolean			increaseDirection = true;
-boolean			twinkleIncrease = true;
-boolean			twinklePause = true;
+bool			glowIncrease, glowDoChange;
+bool			increaseDirection = true;
+bool			twinkleIncrease = true;
+bool			twinklePause = true;
 
 // Color schemes
-enum			color { 
-					rnd, off, white, 
-					warmwhite, red, green, 
+enum			color {
+					rnd, off, white,
+					warmwhite, red, green,
 					blue, yellow, orange
 				};
 
@@ -90,43 +81,47 @@ unsigned long	timer;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, pixelPin, NEO_GRB + NEO_KHZ800);
 
-void readSensors()
-{
+int readSensors() {
+
+	int mode = 0;
+
 	sw1Read = digitalRead(sw1Pin);
 	sw2Read = digitalRead(sw2Pin);
 
-	if( !sw1Read && !sw2Read )	{ currentMode = 1; }
-	if( sw1Read && !sw2Read )	{ currentMode = 2; }
-	if( !sw1Read && sw2Read )	{ currentMode = 3; }
-	if( sw1Read && sw2Read )	{ currentMode = 4; }
+	if(!sw1Read && !sw2Read) mode = 1;
+	if(sw1Read && !sw2Read) mode = 2;
+	if(!sw1Read && sw2Read) mode = 3;
+	if(sw1Read && sw2Read) mode = 4;
+
+	return mode;
 }
 
-int setColor(int color)
-{
-	if(color == rnd) { 
+void setColor(int color) {
+
+	if(color == rnd) {
 		color1 = random(1,255);
 		color2 = random(1,255);
 		color3 = random(1,255);
 
-		if((color1 + color2 + color3) < minColorIntensity) { setColor(rnd); }
+		if((color1 + color2 + color3) < minColorIntensity) setColor(rnd);
 	}
 
-	if(color == off) { color1 = 0; color2 = 0; color3 = 0; }
-	if(color == white) { color1 = 255; color2 = 255; color3 = 255; }
-	if(color == warmwhite) { color1 = 255; color2 = 147; color3 = 41; }
-	if(color == red) { color1 = 255; color2 = 0; color3 = 0; }
-	if(color == green) { color1 = 0; color2 = 255; color3 = 0; }
-	if(color == blue) { color1 = 0; color2 = 0; color3 = 255; }
-	if(color == yellow) { color1 = 255; color2 = 255; color3 = 0; }
-	if(color == orange) { color1 = 255; color2 = 128; color3 = 0; }
+	else if(color == white) { color1 = 255; color2 = 255; color3 = 255; }
+	else if(color == warmwhite) { color1 = 255; color2 = 147; color3 = 41; }
+	else if(color == red) { color1 = 255; color2 = 0; color3 = 0; }
+	else if(color == green) { color1 = 0; color2 = 255; color3 = 0; }
+	else if(color == blue) { color1 = 0; color2 = 0; color3 = 255; }
+	else if(color == yellow) { color1 = 255; color2 = 255; color3 = 0; }
+	else if(color == orange) { color1 = 255; color2 = 128; color3 = 0; }
+	else { color1 = 0; color2 = 0; color3 = 0; }
 }
 
-int setAll(int color, int brightness)
-{
+void setAll(int color, int brightness) {
+
 	if(currentMode != previousMode) {
 		setColor(color);
-		for(int a = 0; a <= numPixels; a++) {
-			strip.setPixelColor(a, color1, color2, color3);	
+		for(int a = 0; a < numPixels; a++) {
+			strip.setPixelColor(a, color1, color2, color3);
 		}
 		strip.setBrightness(brightness);
 		strip.show();
@@ -134,15 +129,17 @@ int setAll(int color, int brightness)
 	}
 }
 
-int worm(int color)
-{
+void worm(int color) {
+
 	if(currentMode != previousMode) { setColor(color); previousMode = currentMode; }
 
-	if(millis() > timer + wormInterval)
-	{
-		for(int a=0; a <= numPixels; a++) { strip.setPixelColor(a,0,0,0); }
+	if(millis() > timer + wormInterval) {
+
+		for(int a=0; a < numPixels; a++) { strip.setPixelColor(a,0,0,0); }
 		for(int b=wormStartPixel; b < wormStartPixel + wormLength; b++) {
-			if(b >= numPixels) { ledNumber = b - numPixels; } else { ledNumber = b; }
+			if(b >= numPixels) ledNumber = b - numPixels;
+			else ledNumber = b;
+
 			strip.setPixelColor(ledNumber, color1, color2, color3);
 		}
 
@@ -150,22 +147,23 @@ int worm(int color)
 		strip.show();
 
 		wormStartPixel++;
-		if(wormStartPixel >= numPixels) { wormStartPixel = 0; }
+		if(wormStartPixel >= numPixels) wormStartPixel = 0;
 		timer = millis();
 	}
 }
 
-int flame(int color)
-{
+void flame(int color) {
+
 	if(currentMode != previousMode) {
 		setColor(color);
 		previousMode = currentMode;
 		flameInterval = random(flameIntervalMin, flameIntervalMax);
 	}
 
-	if(millis() > timer + flameInterval)
-	{
-		for(int a=0; a <= numPixels; a++) { strip.setPixelColor(a, color1, color2, color3); }
+	if(millis() > timer + flameInterval) {
+		for(int a=0; a < numPixels; a++) {
+			strip.setPixelColor(a, color1, color2, color3);
+		}
 		strip.setBrightness(random(minBrightness,maxBrightness));
 		strip.show();
 		flameInterval = random(flameIntervalMin, flameIntervalMax);
@@ -173,25 +171,25 @@ int flame(int color)
 	}
 }
 
-int increase(int color)
-{
-	if(currentMode != previousMode) { 
+void increase(int color) {
+
+	if(currentMode != previousMode) {
 		setColor(color);
 		currentBrightness = maxBrightness;
 		increaseLength = 1;
-		previousMode = currentMode; 
+		previousMode = currentMode;
 	}
 
-	if(millis() > timer + increaseInterval && increaseDirection)
-	{
-		for(int a=0; a <= numPixels; a++) { strip.setPixelColor(a, 0, 0, 0); }
-		for(int a=0; a < increaseLength; a++) { 
-			strip.setPixelColor(a, color1, color2, color3); 
+	if(millis() > timer + increaseInterval && increaseDirection) {
+		for(int a=0; a < numPixels; a++) { strip.setPixelColor(a, 0, 0, 0); }
+
+		for(int a=0; a < increaseLength; a++) {
+			strip.setPixelColor(a, color1, color2, color3);
 		}
 
 		increaseLength++;
-		if(increaseLength >= numPixels) { 
-			increaseLength = 1; 
+		if(increaseLength >= numPixels) {
+			increaseLength = 1;
 			increaseDirection = !increaseDirection;
 		}
 
@@ -200,44 +198,44 @@ int increase(int color)
 		timer = millis();
 	}
 
-	if(millis() > timer + increaseInterval && !increaseDirection)
-	{
-		for(int a=0; a <= numPixels; a++) { 
-			strip.setPixelColor(a, color1, color2, color3); 
+	if(millis() > timer + increaseInterval && !increaseDirection) {
+
+		for(int a=0; a < numPixels; a++) {
+			strip.setPixelColor(a, color1, color2, color3);
 		}
+
 		for(int a=0; a < increaseLength; a++) { strip.setPixelColor(a, 0, 0, 0); }
 
 		increaseLength++;
 
-		if(increaseLength >= numPixels) { 
-			increaseLength = 1; 
+		if(increaseLength >= numPixels) {
+			increaseLength = 1;
 			increaseDirection = !increaseDirection;
-			if(color == rnd && moreRandom) { setColor(color); } 
+			if(color == rnd && moreRandom) setColor(color);
 		}
-		
+
 		strip.setBrightness(currentBrightness);
 		strip.show();
 		timer = millis();
 	}
 }
 
-int twinkle(int color)
-{
-	if(currentMode != previousMode) { 
+void twinkle(int color) {
+
+	if(currentMode != previousMode) {
 		setColor(color);
-		twinkleCurrentPixel = random(0,numPixels);
+		twinkleCurrentPixel = random(0, numPixels);
 		currentBrightness = minBrightness;
-		for(int a=0; a <= numPixels; a++) { strip.setPixelColor(a, 0, 0, 0); }
+		for(int a=0; a < numPixels; a++) { strip.setPixelColor(a, 0, 0, 0); }
 		strip.setBrightness(currentBrightness);
 		strip.show();
-		previousMode = currentMode; 
+		previousMode = currentMode;
 	}
 
-	if(millis() > timer + twinkleInterval && twinkleIncrease && !twinklePause)
-	{
+	if(millis() > timer + twinkleInterval && twinkleIncrease && !twinklePause) {
 		currentBrightness++;
 
-		strip.setPixelColor(twinkleCurrentPixel, color1, color2, color3); 
+		strip.setPixelColor(twinkleCurrentPixel, color1, color2, color3);
 		strip.setBrightness(currentBrightness);
 		strip.show();
 
@@ -245,18 +243,16 @@ int twinkle(int color)
 		timer = millis();
 	}
 
-	if(millis() > timer + twinklePauseInterval && twinkleIncrease && twinklePause)
-	{ 
+	if(millis() > timer + twinklePauseInterval && twinkleIncrease && twinklePause) {
 		twinklePause = false;
 		twinkleIncrease = false;
 		timer = millis();
 	}
 
-	if(millis() > timer + twinkleInterval && !twinkleIncrease && !twinklePause)
-	{
+	if(millis() > timer + twinkleInterval && !twinkleIncrease && !twinklePause) {
 		currentBrightness--;
 
-		strip.setPixelColor(twinkleCurrentPixel, color1, color2, color3); 
+		strip.setPixelColor(twinkleCurrentPixel, color1, color2, color3);
 		strip.setBrightness(currentBrightness);
 		strip.show();
 
@@ -264,68 +260,69 @@ int twinkle(int color)
 		timer = millis();
 	}
 
-	if(millis() > timer + twinklePauseInterval && !twinkleIncrease && twinklePause)
-	{
+	if(millis() > timer + twinklePauseInterval && !twinkleIncrease && twinklePause) {
 		twinklePause = false;
 		twinkleIncrease = true;
 		twinkleCurrentPixel = random(0,numPixels);
-		if(color == rnd && moreRandom) { setColor(color); } 
+		if(color == rnd && moreRandom) setColor(color);
 		timer = millis();
 	}
 }
 
-int glow(int color)
-{
-	if(currentMode != previousMode) { setColor(color); previousMode = currentMode; }
+void glow(int color) {
 
-	if(millis() > timer + glowInterval)
-	{
-		for(int a=0; a <= numPixels; a++)
-		{
+	if(millis() > timer + glowInterval) {
+
+		if(currentMode != previousMode) {
+			setColor(color);
+			previousMode = currentMode;
+		}
+
+		for(int a=0; a < numPixels; a++) {
+
 			glowIncrease = random(2);
 			glowDoChange = random(2);
 
 			if(glowIncrease && glowDoChange) {
-				glowDiv[a] = glowDiv[a] + glowDivDiff; 
-				if(glowDiv[a] > glowDivMax) { glowDiv[a] = glowDivMax; }
+				glowDiv[a] = glowDiv[a] + glowDivDiff;
+				if(glowDiv[a] > glowDivMax) glowDiv[a] = glowDivMax;
 			}
 
 			else if (!glowIncrease && glowDoChange) {
 				glowDiv[a] = glowDiv[a] - glowDivDiff;
-				if(glowDiv[a] < glowDivMin) { glowDiv[a] = glowDivMin; }
+				if(glowDiv[a] < glowDivMin) glowDiv[a] = glowDivMin;
 			}
-			
+
 			strip.setPixelColor(a,
 			(color1 * glowDiv[a]) / glowMultiplier,
 			(color2 * glowDiv[a]) / glowMultiplier,
-			(color3 * glowDiv[a]) / glowMultiplier
-			); 
+			(color3 * glowDiv[a]) / glowMultiplier);
 		}
 
 		strip.setBrightness(maxBrightness);
 		strip.show();
-		timer = millis(); 
+		timer = millis();
 	}
 }
 
-void setup()
-{
+void setup() {
+
 	randomSeed(analogRead(A0));
 
 	strip.begin();
 	strip.show();
 
 	pinMode(pixelPin, OUTPUT);
-	pinMode(sw1Pin, INPUT);
-	pinMode(sw2Pin, INPUT);
+	pinMode(sw1Pin, INPUT_PULLUP);
+	pinMode(sw2Pin, INPUT_PULLUP);
 }
 
-void loop()
-{ 
-	readSensors();
+void loop() {
 
-	if(currentMode == 1) { setAll(warmwhite, 255); }
-	if(currentMode == 2) { setAll(yellow, 150); }
-	if(currentMode == 3) { glow(rnd); }
-	if(currentMode == 4) { twinkle(rnd); }
+	currentMode = readSensors();
+
+	if(currentMode == 1) setAll(warmwhite, 255);
+	if(currentMode == 2) setAll(rnd, 150);
+	if(currentMode == 3) glow(red);
+	if(currentMode == 4) twinkle(rnd);
 }
